5624bc1c12827a605e9a11d430f42563878d328b
Yash Thakkar <thakkaryash21@gmail.com>
2024-06-17 17:19:06 +0530

    testing get

--- appwrite/function.js
+++ appwrite/function.js
@@ -10,6 +10,8 @@ import { storage } from './index.js';
  * @property {(msg: any) => void} error - The error function.
  */
 
+const BUCKET_ID = "666996a9000f8aa81832";
+
 /**
  * The main function that handles the HTTP trigger.
  * @param {Context} context - The context object.
@@ -31,21 +33,22 @@ export default async function main({ req, res, log, error }) {
 
     if (req.method === 'GET') {
         // return the file created in the POST request
-        const files = await storage.listFiles("666996a9000f8aa81832");
-        const file = files.files[0];
-        if (file) {
-            const fileData = await storage.getFileView("666996a9000f8aa81832", file.$id);
-            return res.send(fileData, 200);
-        } else {
-            error('File not found');
-            return res.send("File not found", 404);
-        }
+        // const files = await storage.listFiles(BUCKET_ID);
+        // const file = files.files[0];
+        // if (file) {
+        //     const fileData = await storage.getFileView(BUCKET_ID, file.$id);
+        //     return res.send(fileData, 200);
+        // } else {
+        //     error('File not found');
+        //     return res.send("File not found", 404);
+        // }
+        return res.send("GET method not implemented", 200);
     }
 
     if (req.method === 'POST') {
         const OTA_FILE_BYTES = req.bodyRaw;
         const OTA_FILE = new File(new Blob([OTA_FILE_BYTES]), 'firmware.ota', { type: 'application/octet-stream' });
-        const file = await storage.createFile("666996a9000f8aa81832", ID.unique(), OTA_FILE);
+        const file = await storage.createFile(BUCKET_ID, ID.unique(), OTA_FILE);
 
         if (file.$id) {
             log('File created successfully');
